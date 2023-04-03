be20888707b00d1de5d80b064307ab1ab21f3fcf
UniAlessandroP <alessandro.perfetti@alumni.uniroma2.eu>
2023-04-03 17:17:09 +0200

    [ISW2JT-4] Added JUnit to Maven and created first class to test

---
+++ ISW2hwTicketJira/src/test/control/TicketRetrievalControlTest.java
@@ -0,0 +1,14 @@
+package test.control;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TicketRetrievalControlTest {
+    
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
