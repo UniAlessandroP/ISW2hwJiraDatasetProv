0a3db77ef0e0dabaa51e1d569a4ef96666f5402c
UniAlessandroP <alessandro.perfetti@alumni.uniroma2.eu>
2023-04-04 16:14:58 +0200

    [ISW2JT-4] Added base JUnit 5 interface, basic test for TicketsRetrievalControl and fixed pom issues - closes #6

--- ISW2hwTicketJira/src/test/control/TicketRetrievalControlTest.java
+++ ISW2hwTicketJira/src/test/control/TicketRetrievalControlTest.java
@@ -2,13 +2,45 @@ package test.control;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import jiraticketsmanager.control.TicketRetrievalControl;
+import jiraticketsmanager.entity.Ticket;
+
 class TicketRetrievalControlTest {
+    private static TicketRetrievalControl trc;
+    
+    @BeforeAll
+    static void TicketRetrievalControlBeforeAll() {
+        System.out.println("Beforeall statement");
+        trc = new TicketRetrievalControl();
+        assertNotNull(trc);
+    }
     
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void retrieveTicketsTest() {
+        String[] correctprojnames = {"BOOKKEEPER", "SYNCOPE"};
+        String[] wrongprojnames = {"BOO", "SSYNCOPE", "", null};
+
+        for(String proj : correctprojnames) {
+            List<Ticket> tks = trc.retrieveTickets(proj);
+            assertNotEquals(null, tks);
+            if(!tks.isEmpty()) {
+                for(Ticket tk : tks) {
+                    assertNotNull(tk.getId());
+                    assertTrue(tk.getId() > -1);
+                }
+            }
+        }
+        
+
+        for(String proj : wrongprojnames) {
+            List<Ticket> tks = trc.retrieveTickets(proj);
+            assertEquals(null, tks);
+        }
     }
 
 }
